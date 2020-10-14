202ec38ac35128e3d4c44b0e49ea766b0eea6609
Jay <farinloyejonathan@gmail.com>
2020-10-14 16:33:36 +0100

    Added zap logger. Modified error handling

--- document/controllers.go
+++ document/controllers.go
@@ -1,8 +1,9 @@
 package document
 
 import (
-	"bookateria-api-go/core"
+	"bookateria-api-go/log"
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
@@ -19,7 +20,7 @@ func GetDocuments(w http.ResponseWriter, _ *http.Request) {
 	// Load data from DB
 	db.Find(&documents)
 	err := json.NewEncoder(w).Encode(documents)
-	core.ErrHandler(err)
+	log.Handler("warning", "JSON encoder error", []string{fmt.Sprintf("%v", err)})
 	return
 }
 
@@ -29,37 +30,36 @@ func GetDocument(w http.ResponseWriter, r *http.Request) {
 	documentID := params["id"]
 	db.First(&document, documentID)
 	err := json.NewEncoder(w).Encode(document)
-	core.ErrHandler(err)
+	log.Handler("warning", "JSON encoder error", []string{fmt.Sprintf("%v", err)})
 	return
 }
 
 func PostDocument(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewDecoder(r.Body).Decode(&document)
-	core.ErrHandler(err)
+	log.Handler("warning", "JSON decoder error", []string{fmt.Sprintf("%v", err)})
 	db.Create(&document)
 	err = json.NewEncoder(w).Encode(document)
-	core.ErrHandler(err)
+	log.Handler("warning", "JSON encoder error", []string{fmt.Sprintf("%v", err)})
 	return
 }
 
 func UpdateDocument(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewDecoder(r.Body).Decode(&document)
-	core.ErrHandler(err)
+
+	log.Handler("warning", "JSON decoder error", []string{fmt.Sprintf("%v", err)})
 	db.Save(&document)
 	err = json.NewEncoder(w).Encode(document)
-	core.ErrHandler(err)
+	log.Handler("warning", "JSON encoder error", []string{fmt.Sprintf("%v", err)})
 }
 
 func DeleteDocument(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	id := params["id"]
 	idInUint, _ := strconv.ParseUint(id, 10, 64)
 	idToDelete := uint(idInUint)
 	db.Where("document_id = ?", idToDelete).Delete(&Document{})
-	//err := json.NewEncoder(w).Encode(documents)
-	//core.ErrHandler(err)
 	w.WriteHeader(http.StatusNoContent)
+	log.Handler("info", "Document deleted", []string{fmt.Sprintf("%v", id)})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/gorilla/mux v1.8.0
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/driver/postgres v1.0.2
