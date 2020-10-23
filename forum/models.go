a4fe2b81e8fbadf5d3b1a10fb9532b789768ea46
Jonathan Farinloye <farinloyejonathan@gmail.com>
2020-10-23 20:59:26 +0100

    Ported forum models. Look into readjusting upvotes

--- forum/models.go
+++ forum/models.go
@@ -1,1 +1,41 @@
 package forum
+
+import (
+	"gorm.io/gorm"
+	"os/user"
+)
+
+type QuestionTag struct {
+	gorm.Model
+	Name	string	`json:"name"`
+}
+
+type Question struct {
+	gorm.Model
+	Title			string			`json:"title"`
+	Description		string			`json:"description"`
+	Tags			[]QuestionTag	`json:"tags"`
+	AcceptedAnswer	string			`json:"accepted_answer"`
+	User			user.User		`json:"user"`
+	UpVotes			int				`json:"up_votes"`
+}
+
+type Answer struct {
+	gorm.Model
+	Question	Question	`json:"question"`
+	Response	string		`json:"response"`
+	UpVotes		string		`json:"up_votes"`
+	User		user.User	`json:"user" gorm:"constraints:OnDelete:SET NULL"`
+}
+
+type QuestionUpVote struct {
+	gorm.Model
+	Question	Question	`json:"question"`
+	User 		user.User	`json:"user" gorm:"constraints:OnDelete:CASCADE"`
+}
+
+type AnswerUpvote struct {
+	gorm.Model
+	Answer	Answer 		`json:"answer"`
+	User	user.User	`json:"user" gorm:"constraints:OnDelete:CASCADE"`
+}
