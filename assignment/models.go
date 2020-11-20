c13946dcbab2351630e1ab89035e342c10177dd8
Jonathan Farinloye <farinloyejonathan@gmail.com>
2020-11-20 22:37:45 +0100

    Added base assignment models

--- assignment/models.go
+++ assignment/models.go
@@ -1,1 +1,22 @@
 package assignment
+
+import (
+	"bookateria-api-go/account"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Question struct {
+	gorm.Model
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Deadline    time.Time `json:"deadline"`
+	User		account.User	`json:"user"`
+}
+
+type Submission struct {
+	gorm.Model
+	Question	Question	`json:"question"`
+	User 		account.User	`json:"user"`
+	FileSlug	string		`json:"file_slug"`
+}
