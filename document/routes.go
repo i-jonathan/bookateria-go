d5573ed46f21fc9c10b07f6d2816a934888dcfce
yoruba-codigy <dammysteve10@gmail.com>
2021-01-02 07:14:35 +0000

    Changed Request Type For Update From POST To PUT

--- document/routes.go
+++ document/routes.go
@@ -6,7 +6,7 @@ func Router(router *mux.Router) *mux.Router {
 	router.HandleFunc("", GetDocuments).Methods("GET")
 	router.HandleFunc("/{id}", GetDocument).Methods("GET")
 	router.HandleFunc("", PostDocument).Methods("POST")
-	router.HandleFunc("/{id}", UpdateDocument).Methods("POST")
+	router.HandleFunc("/{id}", UpdateDocument).Methods("PUT")
 	router.HandleFunc("/{id}", DeleteDocument).Methods("DELETE")
 
 	return router
